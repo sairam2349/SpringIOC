52d08d4921cecf5c90c95dc0366784329b1fd47d
sairam2349 <sairam2349@gmail.com>
2023-11-01 13:48:06 -0400

    Setter DI Secondary Types Module

---
+++ Setter_DI_SecondaryTypes/src/main/java/org/example/beans/Engine.java
@@ -0,0 +1,18 @@
+package org.example.beans;
+
+public class Engine {
+
+    private int modelYear;
+
+    public Engine(){
+        System.out.println("Engine");
+    }
+
+    public void setModelYear(int modelYear){
+        this.modelYear = modelYear;
+    }
+
+    public int getModelYear(){
+        return modelYear;
+    }
+}
